ad47ea46a438c0d0a443641d24720d4b2b03ef3d
thfield <thfield@gmail.com>
2013-12-04 09:54:29 -0800

    add forgotten files

---
+++ apple-data.js
@@ -0,0 +1,209 @@
+var dataString = [
+    {
+        "itemName": "iPhone 5s",
+        "options": [
+            {
+                "optionName": "16GB",
+                "optionPrice": "199"
+                },
+            {
+                "optionName": "32GB",
+                "optionPrice": "299"
+                },
+            {
+                "optionName": "64GB",
+                "optionPrice": "399"
+                }
+            ]
+        },
+    {
+        "itemName": "iPhone 5c",
+        "options": [
+            {
+                "optionName": "16GB",
+                "optionPrice": "99"
+                },
+            {
+                "optionName": "32GB",
+                "optionPrice": "199"
+                }
+            ]
+        },
+    {
+        "itemName": "iPhone 4s",
+        "options": [
+            {
+                "optionName": "16GB",
+                "optionPrice": "0"
+                }
+            ]
+        },
+    {
+        "itemName": "Macbook Air 11\"",
+        "options": [
+            {
+                "optionName": "128GB",
+                "optionPrice": "999"
+                },
+            {
+                "optionName": "256GB",
+                "optionPrice": "1199"
+                }
+            ]
+        },
+    {
+        "itemName": "Macbook Air 13\"",
+        "options": [
+            {
+                "optionName": "128GB",
+                "optionPrice": "1099"
+                },
+            {
+                "optionName": "256GB",
+                "optionPrice": "1299"
+                }
+            ]
+        },
+    {
+        "itemName": "Macbook Pro 13\"",
+        "options": [
+            {
+                "optionName": "2.5GHz",
+                "optionPrice": "1199"
+                }
+            ]
+        },
+    {
+        "itemName": "Macbook Pro Retina 13\"",
+        "options": [
+            {
+                "optionName": "128GB",
+                "optionPrice": "1299"
+                },
+            {
+                "optionName": "256GB",
+                "optionPrice": "1499"
+                },
+            {
+                "optionName": "512GB",
+                "optionPrice": "1799"
+                }
+            ]
+        },
+    {
+        "itemName": "Macbook Pro Retina 15\"",
+        "options": [
+            {
+                "optionName": "256GB",
+                "optionPrice": "1999"
+                },
+            {
+                "optionName": "512GB",
+                "optionPrice": "2599"
+                }
+            ]
+        },
+    {
+        "itemName": "Macbook Mini",
+        "options": [
+            {
+                "optionName": "500GB",
+                "optionPrice": "599"
+                },
+            {
+                "optionName": "1TB",
+                "optionPrice": "799"
+                }
+            ]
+        },
+    {
+        "itemName": "iPad Air",
+        "options": [
+            {
+                "optionName": "16GB",
+                "optionPrice": "499"
+                },
+            {
+                "optionName": "32GB",
+                "optionPrice": "599"
+                },
+            {
+                "optionName": "64GB",
+                "optionPrice": "699"
+                },
+            {
+                "optionName": "128GB",
+                "optionPrice": "799"
+                }
+            ]
+        },
+    {
+        "itemName": "iPad Mini",
+        "options": [
+            {
+                "optionName": "WiFi",
+                "optionPrice": "399"
+                },
+            {
+                "optionName": "WiFi/3G",
+                "optionPrice": "529"
+                }
+            ]
+        },
+    {
+        "itemName": "iPad Mini Retina w/LTE",
+        "options": [
+            {
+                "optionName": "16GB",
+                "optionPrice": "529"
+                },
+            {
+                "optionName": "32GB",
+                "optionPrice": "629"
+                },
+            {
+                "optionName": "64GB",
+                "optionPrice": "729"
+                },
+            {
+                "optionName": "128GB",
+                "optionPrice": "829"
+                }
+            ]
+        },
+    {
+        "itemName": "iPad Mini Retina",
+        "options": [
+            {
+                "optionName": "16GB",
+                "optionPrice": "399"
+                },
+            {
+                "optionName": "32GB",
+                "optionPrice": "499"
+                },
+            {
+                "optionName": "64GB",
+                "optionPrice": "599"
+                },
+            {
+                "optionName": "128GB",
+                "optionPrice": "699"
+                }
+            ]
+        },
+    {
+        "itemName": "iPad Mini",
+        "options": [
+            {
+                "optionName": "WiFi",
+                "optionPrice": "299"
+                },
+            {
+                "optionName": "WiFi/3G",
+                "optionPrice": "429"
+                }
+            ]
+        }
+
+    ];
